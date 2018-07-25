a70e9d128d60b716e3597548c464f281fe5b0908
SebastianCerquera <sebastian2zen@gmail.com>
2018-07-24 20:29:26 -0500

    i am realising as it is

--- client-docker/build.sh
+++ client-docker/build.sh
@@ -11,7 +11,7 @@ sudo docker build -t pandora/dev:$DEV_VERSION .
 sudo docker run --rm -v $HOME/.m2/:/root/.m2/ -v $BASE/client:/root/workspace -it pandora/dev:$DEV_VERSION compile
 
 cd $BASE/client
-VERSION=$(cat pom.xml | perl -ne '/<version>([\d\.]+)<\/version>/ && print $1')
+VERSION=$(cat pom.xml | perl -e '$s = 1; while(<>){print $_ if($s); $s=0 if(/parent/); $s=1 if(/\/parent/); }' | perl -ne '/<version>([\d\.]+)(RELEASE)?<\/version>/ && print "$1$2"')
 
 cd $BASE/client-docker
 cp $BASE/client/target/client-$VERSION.jar client.jar
