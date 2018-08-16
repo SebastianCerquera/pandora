fee45df0167a8e36fd1a702c398a39f559e30888
SebastianCerquera <sebastian2zen@gmail.com>
2018-08-16 21:44:43 +0000

    the solver uses a thread per modulus, every modulus iterates over the primes found by the cpu

--- solver/main.cpp
+++ solver/main.cpp
@@ -22,31 +22,44 @@ void checkModulus(uint64_t * primes, mpz_t base, size_t lenMin, size_t lenMax)
         }
     }
 }
+
+void checkModulusAll(uint64_t * primes, mpz_t* base, size_t lenMin, size_t lenMax, int size)
+{
+    std::thread myThreads[size];
+    for(int i = 0; i < size; i++){
+            myThreads[i] = std::thread(checkModulus, primes,  base[i], lenMin, lenMax);
+    }
+
+    for(int i = 0; i < size; i++){
+            myThreads[i].join();
+    } 
+}
  
 int main(int argc, char* argv[])
 {
-    mpz_t result, base;
-    mpz_inits(result, base, NULL);
-    mpz_set_str(base, argv[3], 10);
- 
+    mpz_t result;
     std::size_t x;
+    mpz_inits(result, NULL);
+
+    int size = std::stoul(argv[3], &x, 10);
+    mpz_t base[size];
+    for(int i = 0; i < size; i++){
+            mpz_inits(base[i], NULL);
+            mpz_set_str(base[i], argv[i+4], 10);
+    }
+ 
     uint64_t bottom = std::stoul(argv[1], &x, 10);
     uint64_t top = std::stoul(argv[2], &x, 10);
  
     size_t len;
- 
     uint64_t * primes = CudaSieve::getHostPrimes(bottom, top, len);
     size_t interval = (len - (len % 4))/4;
- 
-    std::thread t1(checkModulus, primes, base, 0*interval, 1*interval);
-    std::thread t2(checkModulus, primes, base, 1*interval, 2*interval);
-    std::thread t3(checkModulus, primes, base, 2*interval, 3*interval);
-    std::thread t4(checkModulus, primes, base, 3*interval, 4*interval + (len % 4));
- 
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
+
+
+    checkModulusAll(primes, base, 0*interval, 1*interval);
+    checkModulusAll(primes, base, 1*interval, 2*interval);
+    checkModulusAll(primes, base, 2*interval, 3*interval);
+    checkModulusAll(primes, base, 3*interval, 4*interval + (len % 4));
     
     cudaFreeHost(primes);            // must be freed with this call b/c page-locked memory is used.
     return 0;
