56344f42a9f79c79565e1058ce86c0c90694ee38
Sebastian Gonzalez Cerquera <sgonzalez@easysol.net>
2018-08-29 14:01:37 -0500

    updates version string, it was failing to compute the version

--- server-docker/push.sh
+++ server-docker/push.sh
@@ -7,7 +7,7 @@ SOURCE_PATH=server/complete
 CREDENTIALS=/tmp/credentials
 
 cd $BASE/$SOURCE_PATH
-VERSION=$(cat pom.xml | perl -e '$s = 1; while(<>){print $_ if($s); $s=0 if(/parent/); $s=1 if(/\/parent/); }' | perl -ne '/<version>([\d\.]+)(RELEASE)?<\/version>/ && print "$1$2"')
+VERSION=$(cat pom.xml | perl -e '$s = 1; while(<>){print $_ if($s); $s=0 if(/<parent>/);}' | perl -ne '/<version>([\d\.]+)(RELEASE)?<\/version>/ && print "$1$2"')
 
 docker tag pandora/server:$VERSION thepandorasys/server:$VERSION
 
