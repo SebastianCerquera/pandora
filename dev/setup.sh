b83ef28e9fccb00665034e14265ce900aa071231
Sebastian Gonzalez Cerquera <sgonzalez@easysol.net>
2018-08-29 14:49:58 -0500

    the setup script only creates the pandora user and installs docker

--- dev/setup.sh
+++ dev/setup.sh
@@ -22,28 +22,22 @@ sudo cp /home/ubuntu/.ssh/authorized_keys /home/$NEW_USER/.ssh/authorized_keys
 sudo chown $NEW_USER:$NEW_USER /home/$NEW_USER/.ssh/authorized_keys
 
 
-sudo apt-get remove docker docker-engine docker.io
-sudo apt-get update
-sudo apt-get install -y  \
+apt-get remove docker docker-engine docker.io
+apt-get update
+apt-get install -y  \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
  
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
  
-sudo apt-key fingerprint 0EBFCD88
+apt-key fingerprint 0EBFCD88
  
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
  
-sudo apt-get update
-sudo apt-get install -y docker-ce
-
-
-cd ~
-git clone https://github.com/SebastianCerquera/pandora.git
-cd pandora
-bash ./client-docker/build.sh 0.0.1
+apt-get update
+apt-get install -y docker-ce
