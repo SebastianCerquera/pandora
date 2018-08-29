bc7723eb2e69fb61eb7815fae463dffd0537c74d
Sebastian Gonzalez Cerquera <sgonzalez@easysol.net>
2018-08-29 14:35:42 -0500

    i was evaluting the wrong condition

--- server-docker/push.sh
+++ server-docker/push.sh
@@ -4,7 +4,7 @@ set -e
 
 BASE=$(pwd)
 SOURCE_PATH=server/complete
-CREDENTIALS=/tmp/credentials
+CREDENTIALS=/tmp/credentials/secret
 
 cd $BASE/$SOURCE_PATH
 VERSION=$(cat pom.xml | perl -e '$s = 1; while(<>){print $_ if($s); $s=0 if(/<parent>/);}' | perl -ne '/<version>([\d\.]+)(RELEASE)?<\/version>/ && print "$1$2"')
@@ -12,8 +12,8 @@ VERSION=$(cat pom.xml | perl -e '$s = 1; while(<>){print $_ if($s); $s=0 if(/<pa
 docker tag pandora/server:$VERSION thepandorasys/server:$VERSION
 
 if [ -f  "$CREDENTIALS" ]; then
-    REGISTRY_USER=$(cat $CREDENTIALS/secret | perl -ne 'print $_ if($. == 1)')
-    REGISTRY_PASSWORD=$(cat $CREDENTIALS/secret | perl -ne 'print $_ if($. == 2)')
+    REGISTRY_USER=$(cat $CREDENTIALS | perl -ne 'print $_ if($. == 1)')
+    REGISTRY_PASSWORD=$(cat $CREDENTIALS | perl -ne 'print $_ if($. == 2)')
     echo $REGISTRY_PASSWORD | docker login -u $REGISTRY_USER --password-stdin
     docker push thepandorasys/server:$VERSION
 else
