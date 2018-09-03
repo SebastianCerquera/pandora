b5615fddd15d8280249a1a4796719a038a8b90e7
Sebastian Cerquera <sebastian2zen@gmail.com>
2018-09-03 12:50:50 -0500

    check if recreating the user is cauing the issue

--- dev/development/adduser.sh
+++ dev/development/adduser.sh
@@ -3,12 +3,16 @@
 NEW_USER=$1
 JENKINS_USER=$2
 
-useradd $NEW_USER 
-echo "$NEW_USER:$NEW_USER" | chpasswd
+UID=$(cat /etc/passwd | perl -lane '@F = split(":", $_); print $F[2] if($F[0] =~ "'"$NEW_USER"'")')
 
-mkdir /home/$NEW_USER
-mkdir /home/$NEW_USER/.local
-chown -R $NEW_USER:$NEW_USER /home/$NEW_USER
+if [ -z "$UID" ]; then
+     useradd $NEW_USER 
+     echo "$NEW_USER:$NEW_USER" | chpasswd
+      
+     mkdir /home/$NEW_USER
+     mkdir /home/$NEW_USER/.local
+     chown -R $NEW_USER:$NEW_USER /home/$NEW_USER
+fi
 
 chown $NEW_USER:$NEW_USER /opt
 
--- dev/jenkins/adduser.sh
+++ dev/jenkins/adduser.sh
@@ -2,12 +2,16 @@
 
 JENKINS_USER=$1
 
-useradd $JENKINS_USER
-echo "$JENKINS_USER:$JENKINS_USER" | chpasswd
+UID=$(cat /etc/passwd | perl -lane '@F = split(":", $_); print $F[2] if($F[0] =~ "'"$JENKINS_USER"'")')
 
-mkdir /home/$JENKINS_USER
-mkdir /home/$JENKINS_USER/.local
-chown -R $JENKINS_USER:$JENKINS_USER /home/$JENKINS_USER
+if [ -z "$UID" ]; then
+     useradd $JENKINS_USER
+     echo "$JENKINS_USER:$JENKINS_USER" | chpasswd
+      
+     mkdir /home/$JENKINS_USER
+     mkdir /home/$JENKINS_USER/.local
+     chown -R $JENKINS_USER:$JENKINS_USER /home/$JENKINS_USER
+fi
 
 SGID=$(ls -all /var/run/docker.sock | awk '{print $4}' | perl -ne '/(.*)\n/ && print $1')
 FGID=$(cat /etc/group | perl -ne '/docker:x:(\d+):/ && print $1')
