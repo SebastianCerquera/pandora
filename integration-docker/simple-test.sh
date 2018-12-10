e1d6f54a60ba7b4b9e230f8da610987b903b41ce
Sebastian Gonzalez Cerquera <sgonzalez@easysol.net>
2018-12-10 17:09:25 -0500

    fix typo

--- integration-docker/simple-test.sh
+++ integration-docker/simple-test.sh
@@ -86,7 +86,7 @@ if [ "x$RESULT_CODE" ==  "x200" ]; then
     echo "The problems was succesfully deleted, the client was already sinced"
 fi
 
-There is a cocurrency error, if the runner keeps trying to delete the problem the clients might not sync properlt.
+## There is a cocurrency error, if the runner keeps trying to delete the problem the clients might not sync properlt.
 while [ "x$RESULT_CODE" ==  "x202" ]; do
     RESULT_CODE=$(curl -D - -XDELETE pandora:8080/v1/problems/$ID 2>/dev/null | perl -ne '/HTTP\/1\.\d+\s*(\d+)/ && print $1')
     echo "Deleting problem, waiting for clients to sync."
