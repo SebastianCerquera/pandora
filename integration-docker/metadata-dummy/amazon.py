6900a95c39023bf9e43b7d062e20679408a74794
Sebastian Gonzalez Cerquera <sgonzalez@easysol.net>
2018-12-10 12:02:49 -0500

    only the first client was registering

--- integration-docker/metadata-dummy/amazon.py
+++ integration-docker/metadata-dummy/amazon.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+COUNTER=0
+
 @app.route("/public-hostname")
 def metadata():
-    return 'ec2-XX-XX-XX-XX.us-east-Y.compute.amazonaws.com'
+    global COUNTER
+    COUNTER=COUNTER+1
+    return 'ec2-XX-XX-XX-XX.us-east-' + str(COUNTER) + '.compute.amazonaws.com'
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5200)
