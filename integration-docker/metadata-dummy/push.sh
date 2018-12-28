d1b6bebce6d3cb0a8e21ebe9bcac2bc379e78ec4
Sebastian Gonzalez Cerquera <sgonzalez@easysol.net>
2018-12-28 16:08:14 -0500

    it was missing to define a variable

--- integration-docker/metadata-dummy/push.sh
+++ integration-docker/metadata-dummy/push.sh
@@ -6,6 +6,8 @@ BASE=$(pwd)
 
 docker tag pandora/metadata-dummy:0.0.1 thepandorasys/metadata-dummy:0.0.1
 
+CREDENTIALS=/tmp/credentials/secret
+
 if [ -f  "$CREDENTIALS" ]; then
     REGISTRY_USER=$(cat $CREDENTIALS | perl -ne 'print $_ if($. == 1)')
     REGISTRY_PASSWORD=$(cat $CREDENTIALS | perl -ne 'print $_ if($. == 2)')
